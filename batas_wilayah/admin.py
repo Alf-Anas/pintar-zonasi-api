01f15da1cbcbc81a4a799cf29f27d8e471e613c6
Alf-Anas <alfadila.anas@gmail.com>
2024-11-28 17:09:04 +0700

    update admin for batas wilayah

--- batas_wilayah/admin.py
+++ batas_wilayah/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import BatasWilayah
+from .models import BatasWilayah, BatasWilayahMetadata
 
 admin.site.register(BatasWilayah, admin.ModelAdmin)
+admin.site.register(BatasWilayahMetadata, admin.ModelAdmin)
